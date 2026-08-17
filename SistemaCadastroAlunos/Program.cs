34b5ede610da42e4788dc0fca7e7a8381c6a289d
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-17 19:34:43 -0300

    Refatora lógica e corrige inconsistências do programa

--- SistemaCadastroAlunos/Program.cs
+++ SistemaCadastroAlunos/Program.cs
@@ -3,65 +3,78 @@ int[] idades = new int[10];
 double[] notas1 = new double[10];
 double[] notas2 = new double[10];
 
-int quantidadeAlunos = 0;
-int tentativasErro = 0;
-
-string nome;
-int idade;
-double nota1, nota2;
+string nome = "";
+int idade = 0;
+double nota1 = 0, nota2 = 0;
 
 Console.WriteLine("=================================");
 Console.WriteLine("      CADASTRO DE ALUNOS");
 Console.WriteLine("=================================");
 
-for (int i = 0; i < 10; i++) {
+Console.Write("\nDigite a quantidade de alunos a cadastrar: ");
+int quantidadeAlunos = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < quantidadeAlunos; i++) {
 
     Console.WriteLine($"\nInsira os dados do aluno {i + 1}:");
 
+    bool nomeValido = false;
 
-    while (true) {
+    while (!nomeValido) {
+
         Console.Write("Nome: ");
         nome = Console.ReadLine();
 
         if (!string.IsNullOrWhiteSpace(nome)) {
-            break;
+            nomeValido = true;
         }
-        Console.WriteLine("Nome informado é inválido! Digite novamente.");
+        else {
+            Console.WriteLine("Nome informado é inválido! Digite novamente.");
+        }
     }
 
-    while (true) {
+    bool idadeValida = false;
+
+    while (!idadeValida) {
         Console.Write("Idade: ");
         var idadeDigitada = Console.ReadLine();
 
         if (int.TryParse(idadeDigitada, out idade) && idade > 0) {
-            break;
+            idadeValida = true;
         }
-
-        Console.WriteLine("Idade informada é inválida! Digite novamente.");
+        else {
+            Console.WriteLine("Idade informada é inválida! Digite novamente.");
+        }
     }
 
-    while (true) {
+    bool Nota1Valida = false;
+
+    while (!Nota1Valida) {
 
         Console.Write("Nota 1: ");
         var nota1Digitada = Console.ReadLine();
 
         if (double.TryParse(nota1Digitada, out nota1) && nota1 >= 0 && nota1 <= 10) {
-            break;
+            Nota1Valida = true;
         }
-
-        Console.WriteLine("Nota informada é inválida! Digite novamente.");
+        else {
+            Console.WriteLine("Nota informada é inválida! Digite novamente.");
+        }
     }
 
-    while (true) {
+    bool Nota2Valida = false;
+
+    while (!Nota2Valida) {
 
         Console.Write("Nota 2: ");
         var nota2Digitada = Console.ReadLine();
 
         if (double.TryParse(nota2Digitada, out nota2) && nota2 >= 0 && nota2 <= 10) {
-            break;
+            Nota2Valida = true;
         }
-
-        Console.WriteLine("Nota informada é inválida! Digite novamente.");
+        else {
+            Console.WriteLine("Nota informada é inválida! Digite novamente.");
+        }
     }
 
     nomes[i] = nome;
@@ -70,11 +83,11 @@ for (int i = 0; i < 10; i++) {
     notas2[i] = nota2;
 }
 
-quantidadeAlunos = nomes.Length;
-
 int opcao = -1;
+int tentativasErro = 0;
 
 while (opcao != 0 && tentativasErro < 3) {
+
     Console.WriteLine("");
     Console.WriteLine("=================================");
     Console.WriteLine("          MENU PRINCIPAL         ");
@@ -91,31 +104,36 @@ while (opcao != 0 && tentativasErro < 3) {
     opcao = int.Parse(Console.ReadLine()!);
 
     if (opcao == 1) {
+
         Console.WriteLine("\n=================================");
         Console.WriteLine("             LISTAGEM              ");
         Console.WriteLine("===================================");
+
         for (int i = 0; i < quantidadeAlunos; i++) {
+
             double media = (notas1[i] + notas2[i]) / 2.0;
+
             Console.WriteLine($"Nome: {nomes[i]}");
             Console.WriteLine($"Idade: {idades[i]}");
             Console.WriteLine($"Média: {media:F1}\n");
+
             tentativasErro = 0;
         }
     }
     else if (opcao == 2) {
-        Console.WriteLine("Buscar Aluno...");
-        tentativasErro = 0;
 
         Console.Write("Digite o nome do aluno que deseja buscar: ");
         string nomeBusca = Console.ReadLine();
 
         bool encontrado = false;
 
-        for (int i = 0; i < 10; i++)
-        {
-            if (nomes[i].ToLower() == nomeBusca.ToLower())
-            {
-                Console.WriteLine("====== ALUNO ENCONTRADO ======");
+        for (int i = 0; i < quantidadeAlunos; i++) {
+
+            if (nomes[i].ToLower() == nomeBusca.ToLower()) {
+
+                Console.WriteLine("\n=================================");
+                Console.WriteLine("        ALUNO ENCONTRADO");
+                Console.WriteLine("=================================");
                 Console.WriteLine("Nome: " + nomes[i]);
                 Console.WriteLine("Idade: " + idades[i]);
                 Console.WriteLine("Nota 1: " + notas1[i]);
@@ -129,26 +147,23 @@ while (opcao != 0 && tentativasErro < 3) {
             }
         }
 
-        if (!encontrado)
-        {
+        if (!encontrado) {
             Console.WriteLine("Aluno não encontrado.");
         }
+
+        tentativasErro = 0;
     }
-    else if (opcao == 3)
-    {
+    else if (opcao == 3) {
         Console.WriteLine("\n=================================");
         Console.WriteLine("        ALUNOS APROVADOS");
         Console.WriteLine("=================================");
 
         int totalAprovados = 0;
-        tentativasErro = 0;
 
-        for (int i = 0; i < quantidadeAlunos; i++)
-        {
+        for (int i = 0; i < quantidadeAlunos; i++) {
             double media = (notas1[i] + notas2[i]) / 2.0;
 
-            if (media >= 7)
-            {
+            if (media >= 7) {
                 Console.WriteLine($"{nomes[i]} - Média: {media:F1}");
                 totalAprovados++;
             }
@@ -156,5 +171,44 @@ while (opcao != 0 && tentativasErro < 3) {
 
         Console.WriteLine("---------------------------------");
         Console.WriteLine($"Total de aprovados: {totalAprovados}");
+
+        tentativasErro = 0;
+    }
+    else if (opcao == 4) {
+
+        Console.WriteLine("\n=================================");
+        Console.WriteLine("        MÉDIA DA TURMA");
+        Console.WriteLine("=================================");
+
+        if (quantidadeAlunos > 0) {
+
+            double somaMedias = 0;
+
+            for (int i = 0; i < quantidadeAlunos; i++) {
+
+                double mediaAluno = (notas1[i] + notas2[i]) / 2;
+                somaMedias = somaMedias + mediaAluno;
+            }
+
+            double mediaTurma = somaMedias / quantidadeAlunos;
+            Console.WriteLine($"A média geral da turma é: " + mediaTurma);
+        }
+
+        tentativasErro = 0;
+    }
+    else if (opcao == 0) {
+        Console.WriteLine("Programa encerrado.");
+    }
+    else {
+        tentativasErro = tentativasErro + 1;
+
+        if (tentativasErro >= 3) {
+            Console.WriteLine("Número máximo de tentativas inválidas atingido!");
+            Console.WriteLine();
+            Console.WriteLine("*** Programa Encerrado! ***");
+        }
+        else {
+            Console.WriteLine($"Opção inválida! Tentativa {tentativasErro} de 3. Tente novamente.");
+        }
     }
 }
